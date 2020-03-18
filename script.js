ca098850ef39b092fdc2505442ad7f226986a53c
Timur681 <teekirienkov@gmail.com>
2020-03-18 09:46:16 +0300

    get elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const formSearch = document.querySelector('.form-search'),
+    inputCitiesFrom = document.querySelector('.input__cities-from'),
+    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+    inputCitiesTo = document.querySelector('.input__cities-to'),
+    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
+    inputDateDepart = document.querySelector('.input__date-depart'),
+    buttonSearch = document.querySelector('.button__search');
+
+
