00bd091d16e8352160db70a11ada063122fee3ea
Timur681 <teekirienkov@gmail.com>
2020-03-19 21:40:48 +0300

    Добавил комментарии к коду

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Получение html элементов со страницы
 const formSearch = document.querySelector('.form-search'),
     inputCitiesFrom = document.querySelector('.input__cities-from'),
     dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
@@ -6,31 +7,28 @@ const formSearch = document.querySelector('.form-search'),
     inputDateDepart = document.querySelector('.input__date-depart'),
     buttonSearch = document.querySelector('.button__search');
 
-
+// Массив с городами
 const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск',
     'Керчь', 'Волгоград', 'Самара', 'Днепропетровск', 'Екатеринбург', 'Одесса',
     'Ухань', 'Шымкент', 'Нижний Новгород', 'Калининград', 'Вроцлав', 'Ростав-на-Дону'];
 
+// Функция с алгоритмом живого поиска
 const showCity = (input, list) => {
     list.textContent = '';
     if (input.value !== '' && inputCitiesFrom.value !== null) {
         const filterCity = city.filter((item)=> {
-
             const fixItem = item.toLowerCase();
             return fixItem.includes(input.value.toLowerCase());
-
         });
         filterCity.forEach((item) => {
-
             const li = document.createElement('li');
-            li.classList.add('dropdown__city');
+            li.classList.add('dropdown__city');   // добавления класса в DOM 
             li.textContent = item;
             list.append(li);
-
         });
     }
 };
-// функция выбора города из выпадающего списка
+// Функция выбора города из выпадающего списка (и удаление списка после выбора)
 const selectCity = (event, input, list) => {
     const target = event.target;
     if (target.tagName.toLowerCase() === 'li') {
@@ -38,13 +36,14 @@ const selectCity = (event, input, list) => {
         list.textContent = '';
     }
 };
-
+// Обработчики событий ввода названия города (функция с живым поиском)
 inputCitiesFrom.addEventListener('input', ()=>{
     showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
 inputCitiesTo.addEventListener('input', ()=>{
     showCity(inputCitiesTo, dropdownCitiesTo);
 });
+// Обработчики событий выбора города (функция с выбором из списка и удалением списка)
 dropdownCitiesFrom.addEventListener('click', (event) => {
     selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
 });
