d0d7fed77eafa2b2ba1a07d9af7b36154bb56f7a
Timur681 <teekirienkov@gmail.com>
2020-03-21 14:15:06 +0300

    add logic

--- script.js
+++ script.js
@@ -60,9 +60,67 @@ const selectCity = (event, input, list) => {
         list.textContent = '';
     }
 };
+
+const getNameCity = (code) => {
+    const objCity = city.find((item) => item.code === code);
+    return objCity.name;
+}; 
+
+// Функция которая вызывается в createCard (в html коде) которая выводит кол-во пересадок
+const getChanges = (n) => {
+    if (n) {
+        return n === 1 ? 'С одной пересадкой' : 'С двумя пересадками';
+    } else {
+        return 'Без пересадок';
+    }
+};
+const createCard = (data) => {
+    const ticket = document.createElement('article');      // создание элемента
+    ticket.classList.add('ticket');
+
+    let deep = '';
+
+    if (data) {
+        deep = `
+        <h3 class="agent">${data.gate}</h3>
+        <div class="ticket__wrapper">
+	        <div class="left-side">
+		        <a href="https://www.aviasales.ru/search/SVX2905KGD1" class="button button__buy">Купить
+			            за ${data.value}₽</a>
+	    </div>
+	    <div class="right-side">
+		    <div class="block-left">
+			    <div class="city__from">Вылет из города
+				    <span class="city__name">${getNameCity(data.origin)}</span>
+			    </div>
+			<div class="date">${data.depart_date} г.</div>
+		    </div>
+
+		    <div class="block-right">
+			    <div class="changes">${getChanges(data.number_of_changes)}</div>
+			    <div class="city__to">Город назначения:
+				    <span class="city__name">${getNameCity(data.destination)}</span>
+			    </div>
+		    </div>
+	        </div>
+        </div>
+        `;
+    } else {
+        deep = '<h3>К сожалению билетов на эту дату не нашлось</h3>';
+    }
+
+    ticket.insertAdjacentHTML('afterbegin', deep);
+
+    return ticket;
+};
+
 // Функция билета
 const renderCheapDay = (cheapTicket) => {
-    console.log(cheapTicket);
+    const ticket = createCard(cheapTicket[0]); // так как получаем массив, чтобы получить самый дешевый билет получаем самый первый через индекс 0
+
+    cheapestTicket.append(ticket); // добавление в форму "самый дешевый билет на выбранную дату"
+
+    console.log(ticket);
 };
 // Функция билетов
 const renderCheapYear = (cheapTickets) => {
@@ -115,14 +173,14 @@ formSearch.addEventListener('submit', (event)=>{
         }); // получение города в который летим
 
     const formData = {
-        from: cityFrom.code,       // получение кода города (обращение к объекту)
-        to: cityTo.code,           // тоже самое
+        from: cityFrom,       // получение кода города (обращение к объекту)
+        to: cityTo,           // тоже самое
         when: inputDateDepart.value
     };
 
     if (formData.from && formData.to) {
-        const requestData = `?depart_date=${formData.when}&origin=${formData.from}`+
-        `&destination=${formData.to}&one_way=true`;
+        const requestData = `?depart_date=${formData.when}&origin=${formData.from.code}`+
+        `&destination=${formData.to.code}&one_way=true`;
     
         getData(calendar + requestData, (response)=> {
             renderCheap(response, formData.when);
